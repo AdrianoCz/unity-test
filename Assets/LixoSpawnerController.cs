8b5c955155cf7b5d2a7e6ea1102ff001f09b8eb3
AdrianoCz <235764285+AdrianoCz@users.noreply.github.com>
2026-04-10 16:09:47 -0300

    avbab

---
+++ Assets/LixoSpawnerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LixoSpawnerController : MonoBehaviour
+{
+    public float maximumX;
+    public float fixedY;
+    public float fixedZ;
+    public float timer;
+    public GameObject Lixo;
+    public int MaxPoints;
+    public int points = 0;
+
+    IEnumerator SpawnRoutine() {
+        while(points<MaxPoints){
+            Instantiate(Lixo, new Vector3(Random.Range(-maximumX, maximumX+1), fixedY, fixedZ), Quaternion.identity);
+            yield return new WaitForSeconds(timer);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
